5a38efec1511cd470a63603e50ea8fc263ca7dac
SanjidaRahmanOmi <sanjidaomi92@gmail.com>
2024-01-26 19:20:02 +0600

    Get elemnt which we give as a input in input field as new task

---
+++ To_Do_Project/scirpt.js
@@ -0,0 +1,8 @@
+let form1 = document.getElementById('form1');
+let form2 = document.getElementById('form2');
+
+form1.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let newTaskValue = document.getElementById('task').value;
+
+})

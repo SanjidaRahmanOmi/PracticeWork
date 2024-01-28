ad4dbaf32634fd1a1d35529d8a366326146b4032
SanjidaRahmanOmi <sanjidaomi92@gmail.com>
2024-01-28 21:14:42 +0600

    Adding random background color using DOM

--- BGcolor/script.js
+++ BGcolor/script.js
@@ -1,3 +1,10 @@
-
-
-
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+document.getElementById('btn').addEventListener('click', function () {
+    let randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+});

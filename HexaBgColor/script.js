504ed6373ae661769ff5e9a1d63373efeb9249e6
SanjidaRahmanOmi <sanjidaomi92@gmail.com>
2024-01-28 22:09:39 +0600

    Adding random HexaDecimal  background color using DOM

--- HexaBgColor/script.js
+++ HexaBgColor/script.js
@@ -2,9 +2,12 @@ function getRandomColor() {
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+    return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
 }
+let hexa= document.getElementById('hexa');
 document.getElementById('btn').addEventListener('click', function () {
     let randomColor = getRandomColor();
+    hexa.value = randomColor;
     document.body.style.backgroundColor = randomColor;
+   
 });

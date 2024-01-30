648875d64637831ab87bae0a57c025e468292578
SanjidaRahmanOmi <sanjidaomi92@gmail.com>
2024-01-30 21:56:25 +0600

    create a div for showing copied hexa color

--- HexaBgColor/script.js
+++ HexaBgColor/script.js
@@ -5,12 +5,20 @@ function getRandomColor() {
     return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
 }
 let hexa= document.getElementById('hexa');
-document.getElementById('btn').addEventListener('click', function () {
+document.getElementById('btn').addEventListener('click', (event)=> {
+    event.preventDefault();
     let randomColor = getRandomColor();
     hexa.value = randomColor;
     document.body.style.backgroundColor = randomColor;
    
 });
-document.getElementById('copy').addEventListener('click', function () {
+document.getElementById('copy').addEventListener('click', (event)=> {
+    event.preventDefault();
     navigator.clipboard.writeText(hexa.value);
+    
+     let div = document.createElement('div');
+     div.innerText = hexa.value+ " copied";
+     div.className = 'msg';
+     document.body.appendChild(div);
 });
+
